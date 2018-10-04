14ccd2a652d4def5345b708eaad47f456ed7f676
Star Developer <30364734+fullstackdeveloperstar@users.noreply.github.com>
2018-10-04 18:40:44 +0200

    Login

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,8 +50,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 
-$route['default_controller'] = "login";
-$route['404_override'] = 'login';
+$route['default_controller'] = "Login";
+$route['404_override'] = 'Login';
 
 
 /*********** USER DEFINED ROUTES *******************/
